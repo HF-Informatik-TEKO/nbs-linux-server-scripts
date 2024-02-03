9673b971c957bc9ebb0c8b231c55494430d8fd6f
NRG-Drink <beatdz93@hotmail.com>
2024-02-03 23:51:40 +0100

    chore: add ssh warning

--- staticip/setup.sh
+++ staticip/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "WARNING! changing static ip via ssh will lead to a disconnect."
 read -p "Do you really want to change your static IP? (y|n): " is_process
 if [[ $is_process == null || $is_process != "y" ]]; then
 	exit 0
