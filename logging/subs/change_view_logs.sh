4166d61ff893a7ba9ea21f48d2283c835381c0d3
NRG-Drink <beatdz93@hotmail.com>
2024-02-06 21:34:18 +0100

    chore: make see logs more explicit

--- logging/subs/change_view_logs.sh
+++ logging/subs/change_view_logs.sh
@@ -2,7 +2,7 @@
 # Check existence of log folder
 if [ -d "/var/log/performance/" ]; then
     # Navigate an show content of log folder
-    echo "All available logs:"
+    echo "All available logs: at /var/log/performance/"
     ls /var/log/performance/
 else
     echo "No existing log folder. Setup logging first."
